0f83ba7d9eb49c0eaf4a82116aa15458d450f219
Brad Witte <github@net.distanthaze.com>
2021-09-18 11:18:14 -0500

    add script

---
+++ dyndns.sh
@@ -0,0 +1,68 @@
+# Dynamic DNS
+#
+
+# Config
+dh_api_key=YOUR_KEY
+domain="dyndns.example.com"
+do_ipv4=true
+do_ipv6=true
+
+## No config below here
+
+normalize_ipv6() {
+    python -c "import socket; print(socket.inet_ntop(socket.AF_INET6, socket.inet_pton(socket.AF_INET6, '${1}')))"
+}
+
+get_ip() {
+    curl -"$1" -s "http://bot.whatismyipaddress.com/"
+}
+
+add_dns() {
+    curl -s "$dh_url&cmd=dns-add_record&record=$domain&type=$1&value=$2"
+}
+
+update_dns() {
+    curl -s "$dh_url&cmd=dns-remove_record&record=$domain&type=$1&value=$2"
+    add_dns $1 $3
+}
+
+dyn_dns() {
+    if [ "$1" == "4" ]; then type="A"; else type="AAAA"; fi
+    echo "Checking $type record for $domain"
+    old_ip=$(echo "$dns_records" | grep $domain | grep "\s$type\s"| cut -f 5)
+    new_ip=$(get_ip "$1")
+
+    if [ "$old_ip" == "" ]; then
+        echo "No $type record found, creating one pointing to $new_ip..."
+        add_dns $type $new_ip
+        old_ip=$new_ip
+    else
+        if [ "$1" == "4" ]; then
+            old_ip_comp="$old_ip"
+            new_ip_comp="$new_ip"
+        else
+            old_ip_comp=$(normalize_ipv6 "$old_ip")
+            new_ip_comp=$(normalize_ipv6 "$new_ip")
+        fi
+
+        echo "new ip: $new_ip ($new_ip_comp)"
+        echo "old ip: $old_ip ($old_ip_comp)"
+        if [ "$new_ip_comp" == "$old_ip_comp" ]; then
+            echo "no change"
+        else
+            echo "different, updating..."
+            update_dns $type $old_ip $new_ip
+        fi
+    fi
+}
+
+dh_url="https://api.dreamhost.com/?key=$dh_api_key"
+dns_records=$(curl -s "$dh_url&cmd=dns-list_records")
+
+if [ "$do_ipv4" = true ]; then
+    dyn_dns 4
+fi
+
+if [ "$do_ipv6" = true ]; then
+    dyn_dns 6
+fi
